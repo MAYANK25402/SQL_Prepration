be40474bfcdd9cd4abac80312e93ecb9d617be99
MAYANK25402 <70213883+MAYANK25402@users.noreply.github.com>
2024-03-04 00:57:58 +0530

    Create Where_And.sql

---
+++ Where/Where_And.sql
@@ -0,0 +1,5 @@
+# Select all records where the City column has the value 'Berlin' and the PostalCode column has the value 12209.
+
+SELECT * FROM CUSTOMERS WHERE City = "Berlin" AND PostalCode = 12209;
+
+--> In this query I have used both the WHERE, AND commands to define the data required by the user.
