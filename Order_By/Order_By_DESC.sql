b1e42ce1323b81f3792369627e20dcad6e4a63bb
MAYANK25402 <70213883+MAYANK25402@users.noreply.github.com>
2024-03-04 01:21:38 +0530

    Create Order_By_DESC.sql

---
+++ Order_By/Order_By_DESC.sql
@@ -0,0 +1,5 @@
+# Select all records from the Customers table, sort the result reversed alphabetically by the column City.
+
+SELECT * FROM CUSTOMERS Order By City DESC;
+
+--> This Query will print the result in the descending order as we have used the function DESC to print the value from large to smaller.
