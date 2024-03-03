9d3631e78c63472557d0a893482847c1c73d4894
MAYANK25402 <70213883+MAYANK25402@users.noreply.github.com>
2024-03-04 01:29:14 +0530

    Create Order_By.sql

---
+++ Order_By/Order_By.sql
@@ -0,0 +1,5 @@
+# Select all records from the Customers table, sort the result alphabetically, first by the column Country, then, by the column City.
+
+SELECT * FROM CUSTOMERS Order By Country, City;
+
+--> This query will print the values alphabetically firstly with column Country and then with column City.
