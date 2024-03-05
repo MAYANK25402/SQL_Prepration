04f946e6e1c4e7b1165677dbedd1fac9f4194376
MAYANK25402 <70213883+MAYANK25402@users.noreply.github.com>
2024-03-05 23:05:55 +0530

    Create Inser_Table.sql

---
+++ INSERT/Inser_Table.sql
@@ -0,0 +1,8 @@
+# In this query there is the insertion of the data into the table using Insert function
+
+INSERT INTO Customers (
+  Customer ID, Customer name, DOB, ID Number
+)
+VALUES(
+  "1", "Nissan GTR", "1850", "4"
+);
